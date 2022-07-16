57771dd953071ec8edf95d87f691231233d36b4b
EgorKanunnikov <egor.kanunnikov@bk.ru>
2022-07-16 22:33:13 +0300

    first task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,5 +1,27 @@
+"use strict";
+// этот код работает в современном режиме
+
 function solveEquation(a, b, c) {
-  let arr;
+  let arr = [];
+  return "Ошибка, а не равно 0"
+  let D = b ** 2 - (4 * (a * c));
+  let x1;
+  let x2;
+
+  if (D > 0) {
+    return arr;
+
+  } else if (D === 0) {
+    x1 = -b / (2 * a);
+    return arr[0];
+
+  } else if (D > 0) {
+    x1 = (-b + Math.sqrt(d)) / (2 * a);
+    x2 = (-b - Math.sqrt(d)) / (2 * a);
+    return arr[0, 1]
+  }
+
+
   // код для задачи №1 писать здесь
   return arr; // array
 }
