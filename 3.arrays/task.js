35ade6bda895b332b81360fdc40f2c23e2601508
EgorKanunnikov <egor.kanunnikov@bk.ru>
2022-08-06 11:42:29 +0300

    fifth commit v2.0

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,17 +1,13 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  result = arr1.length === arr2.length && arr1.every((n, i) => n === arr2[i])
-
-  return result; // boolean
+  return arr1.length === arr2.length && arr1.every((n, i) => n === arr2[i]); // boolean
 }
 
 function advancedFilter(arr) {
   let resultArr;
 
-  resultArr = arr.filter(item => item > 0)
+  return arr.filter(item => item > 0)
     .filter(item => item % 3 === 0)
-    .map(item => item * 10)
-
-  return resultArr; // array
+    .map(item => item * 10); // array
 }
